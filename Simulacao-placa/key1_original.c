4f0e2bca771f5edaa95fd6e8f7b301d5815899e7
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2025-01-19 16:12:15 -0300

    feature leds

--- Simulacao-placa/key1_original.c
+++ Simulacao-placa/key1_original.c
@@ -4,7 +4,7 @@
 // Pino GPIO para o buzzer
 #define BUZZER_PIN 10
 
-// Defina os pinos GPIO para as linhas e colunas
+// Pinos GPIO para as linhas e colunas do teclado
 #define ROW_1 16
 #define ROW_2 17
 #define ROW_3 18
@@ -13,6 +13,11 @@
 #define COL_2 4
 #define COL_3 9
 #define COL_4 8
+
+// Pinos GPIO dos LEDs
+#define RED_LED 13
+#define BLUE_LED 12
+#define GREEN_LED 11
 
 // Mapeamento do teclado
 char keys[4][4] = {
@@ -47,6 +52,15 @@ void init_keypad() {
     gpio_pull_down(COL_2);
     gpio_pull_down(COL_3);
     gpio_pull_down(COL_4);
+}
+
+void init_leds() {
+    gpio_init(RED_LED);
+    gpio_init(BLUE_LED);
+    gpio_init(GREEN_LED);
+    gpio_set_dir(RED_LED, GPIO_OUT);
+    gpio_set_dir(BLUE_LED, GPIO_OUT);
+    gpio_set_dir(GREEN_LED, GPIO_OUT);
 }
 
 // Função para digitalizar o teclado
@@ -89,6 +103,7 @@ int main() {
     stdio_init_all();  // Inicializar saída padrão (USB/Serial)
     init_keypad();     // Inicializar o teclado
     init_buzzer();     // Inicializar o buzzer
+    init_leds();;      // Inicializar os LEDs
 
     printf("Pressione uma tecla no teclado matricial.\n");
 
@@ -97,6 +112,27 @@ int main() {
         if (key != '\0') {  // Se uma tecla foi pressionada
             printf("Tecla pressionada: %c\n", key);
             sleep_ms(300);  // Debounce
+        }
+        // LEDs
+        while (key == 'A') { gpio_put(RED_LED, true); } 
+        gpio_put(RED_LED, false);
+        
+        while (key == 'B') { gpio_put(GREEN_LED, true); }
+        gpio_put(GREEN_LED, false);
+
+        while (key == 'C') { gpio_put(BLUE_LED, true); } 
+        gpio_put(BLUE_LED, false);
+
+        while (key == 'D') {
+            gpio_put(RED_LED, true);
+            sleep_ms(1000);
+            gpio_put(RED_LED, false);
+            gpio_put(GREEN_LED, true);
+            sleep_ms(1000);
+            gpio_put(GREEN_LED, false);
+            gpio_put(BLUE_LED, true);
+            sleep_ms(1000);
+            gpio_put(BLUE_LED, false);
         }
     }
 
